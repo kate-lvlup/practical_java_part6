7cf1c5d1a940fbf5c2a0c0931cd062947f67f487
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-18 16:01:57 +0300

    add task4 two methods: 1. readFile(String filename):           Reads the contents of a file that contains binary data as a string (BufferedReader - line by line, merging all lines into one binary data)
              2. binaryToString(String binaryString): Converts a binary data string into text (Splits the binary string into 7-bit groups, converts each group into a character and merges them into the final string)

--- src/main/java/jom/com/softserve/s6/task4/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task4/MyUtils.java
@@ -1,13 +1,37 @@
 package jom.com.softserve.s6.task4;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.Locale;
 
 public class MyUtils {
 
-    //Write your code here
+    public static String readFile(String filename) {
+        StringBuilder binaryStringBuilder = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                binaryStringBuilder.append(line.trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return binaryToString(binaryStringBuilder.toString());
+    }
+
+    private static String binaryToString(String binaryString) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < binaryString.length(); i += 7) {
+            String byteString = binaryString.substring(i, Math.min(i + 7, binaryString.length()));
+            int charCode = Integer.parseInt(byteString, 2);
+            result.append((char) charCode);
+        }
+
+        return result.toString();
+    }
 
 }
