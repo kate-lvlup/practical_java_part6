a029e3a3ff3fa85003fc22a44932133a574c3c70
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-18 15:31:18 +0300

    add task3 converts the character to a binary string -> aligns the string to 7 characters with spaces on the left -> replaces spaces with zeros to get a string of exactly 7 bits

--- src/main/java/jom/com/softserve/s6/task3/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task3/MyUtils.java
@@ -2,10 +2,20 @@ package jom.com.softserve.s6.task3;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 public class MyUtils {
-
-    //Write your code here
-
+    public static void writeFile(String filename, String text) {
+        try (FileOutputStream fos = new FileOutputStream(filename)) {
+            for (char ch : text.toCharArray()) {
+                //The string must be right-aligned within 7 characters. If the string is shorter
+                // than 7 characters, it will be padded with spaces on the left until it reaches 7 characters.
+                String binaryString = String.format("%7s", Integer.toBinaryString(ch)).replace(' ', '0');
+                for (char bit : binaryString.toCharArray()) {
+                    fos.write(bit);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
