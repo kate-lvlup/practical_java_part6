cc85beb310623c7a84f3c9ca62ccc974e289a7ac
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-18 15:19:53 +0300

    add task2 find the date that will be in a given year/month/day

--- src/main/java/jom/com/softserve/s6/task2/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task2/MyUtils.java
@@ -2,11 +2,11 @@ package jom.com.softserve.s6.task2;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 public class MyUtils {
     public static String getDateAfterToday(int years, int months, int days) {
-        //Write code there
-        return "";
+        LocalDate currentDate = LocalDate.now();
+        LocalDate futureDate = currentDate.plusYears(years).plusMonths(months).plusDays(days);
+        return futureDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
