2b4f202f186c90a60cf10f52fd8d67768557af01
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-19 12:36:06 +0300

    add task5 `nameList` method to process names from Map: removed extra spaces-> capitalized- first letter-> other latter- lowercase-> removed duplicates-> sorted final list

--- src/main/java/jom/com/softserve/s6/task5/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task5/MyUtils.java
@@ -2,11 +2,21 @@ package jom.com.softserve.s6.task5;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class MyUtils {
+    public static Stream<String> nameList(Map<String, Stream<String>> map) {
+        if (map == null) {
+            throw new NullPointerException("The map cannot be null.");
+        }
+        return map.values().stream()
+                .filter(Objects::nonNull)
+                .flatMap(stream -> stream.filter(Objects::nonNull)
+                        .map(name -> name.replaceAll("\\s+", "").trim()))
+                .filter(name -> !name.isEmpty())
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase())
+                .distinct()
+                .sorted();
 
-    //Write your code here
-
+    }
 }
