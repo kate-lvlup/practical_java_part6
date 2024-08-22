71a8584ec1148feaf8de18a993dc6866e1c8155b
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-22 14:21:39 +0300

    add task6 best solution this method

--- src/main/java/jom/com/softserve/s6/task6/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task6/MyUtils.java
@@ -2,46 +2,26 @@ package jom.com.softserve.s6.task6;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class MyUtils {
-
-    public static Map<String, Stream<String>> phoneNumbers(List<Stream<String>> list) {
-        Map<String, Set<String>> map = new TreeMap<>();
-
-        for (Stream<String> stream : list) {
-            stream.forEach(phone -> {
-                if (phone == null) {
-                    return;
-                }
-                String cleanPhone = phone.replaceAll("\\D", "").trim();
-                if (cleanPhone.isEmpty()) {
-                    return;
-                }
-                String key;
-                if (cleanPhone.length() >= 10) {
-                    key = cleanPhone.substring(0, 3);
-                    cleanPhone = cleanPhone.substring(3);
-                } else if (cleanPhone.length() > 7) {
-                    key = cleanPhone.substring(0, cleanPhone.length() - 7);
-                    cleanPhone = cleanPhone.substring(cleanPhone.length() - 7);
-                } else {
-                    key = "err";
-                }
-
-                if (cleanPhone.length() == 7 && key.equals("err")) {
-                    key = "loc";
-                }
-
-                map.computeIfAbsent(key, k -> new TreeSet<>()).add(cleanPhone);
-            });
-        }
-
-        return map.entrySet().stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().stream()));
+class MyUtils {
+    public Map<String, Stream<String>> phoneNumbers(List<Stream<String>> list) {
+        return list.stream()
+                .filter(Objects::nonNull)
+                .flatMap(Function.identity())
+                .filter(Objects::nonNull)
+                .filter(s -> s.length() > 1)
+                .map(s -> s.replaceAll("\\D", ""))
+                .map(s -> s.length() == 10 ? s : s.length() == 7 ? "loc".concat(s) : "err".concat(s))
+                .distinct()
+                .sorted()
+                .collect(Collectors.toMap(
+                        e -> e.substring(0, 3),
+                        e -> Stream.of(e.substring(3)),
+                        Stream::concat
+                ));
     }
 }
